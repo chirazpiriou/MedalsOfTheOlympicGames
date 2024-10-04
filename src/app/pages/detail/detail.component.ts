da808559f3c9f4942489f83f19eee7cc03aa431c
chiraz.piriou <chirazpiriou@gmail.com>
2024-10-04 12:26:55 +0200

    Set Line Chart display parameters

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -23,6 +23,21 @@ export class DetailComponent implements OnInit{
   public countryVersusMedalsPerYear:{[country:string]:{year: number,medalsCount: number}[]}={};
   public countrysVersusMedalsPerYearLineChartFormat: { name: string; series: {value:number,name:string}[] }[] = [];
 
+   // options Line chart
+   legend: boolean = false;
+   showLabels: boolean = true;
+   animations: boolean = true;
+   xAxis: boolean = true;
+   yAxis: boolean = true;
+   showYAxisLabel: boolean = true;
+   showXAxisLabel: boolean = true;
+   xAxisLabel: string = 'Year';
+   yAxisLabel: string = 'Population';
+   timeline: boolean = true;
+   view: [number, number] = [800, 300];
+   colorScheme = {
+     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+   };
 
   constructor(private olympicService: OlympicService , private route: ActivatedRoute){}
 
